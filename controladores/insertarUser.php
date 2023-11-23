7cf860a4929daacec38d068bc43b5edd7976ec18
FernandoCamargoUMG <fcamargoa@miumg.edu.gt>
2023-11-23 06:22:46 -0600

    cambio en controlador insertar usuario

--- controladores/insertarUser.php
+++ controladores/insertarUser.php
@@ -14,14 +14,14 @@ require '../config/conexion.php';
     echo '
 				<script language="javascript">
 					alert("Registro creado correctamente");
-					window.location.replace("http://localhost/inccav/menu_principal.php");
+					window.location.replace("https://colegioinccav.online/menu_principal.php");
 				</script>
 			';
     } else {
     echo '
 				<script language="javascript">
 					alert("Error al  crear el registro");
-					window.location.replace("http://localhost/inccav/menu_principal.php");
+					window.location.replace("https://colegioinccav.online/menu_principal.php");
 				</script>
 			';
     }
