462919c321c244187e4588b82686c0bfd8dba266
FernandoCamargoUMG <fcamargoa@miumg.edu.gt>
2023-11-22 21:51:38 -0600

    se hicieron cambios

--- menu_principal.php
+++ menu_principal.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Bienvenido Menu Principal</title>
+	<title>Bienvenido al menu Principal</title>
 	<link rel="stylesheet" href="./estilos/estilos.css">
 </head>
 
